73e1d9aa15521dfeff6fd63a81ecfda020b77ff2
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-01-12 11:02:35 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+// while True:
+//     print("Sound level: " + input.sound_level())
+//     if input.sound_level() > 150:
+//         light.show_animation(light.rainbowAnimation, 500)
+//     else:
+//         light.clear()
 while (true) {
-    console.log("Sound level: " + input.soundLevel())
-    if (input.soundLevel() > 150) {
-        light.showAnimation(light.rainbowAnimation, 500)
+    console.log("Light Level: " + input.lightLevel())
+    if (input.lightLevel() > 9) {
+        music.powerUp.play()
     } else {
         light.clear()
     }
