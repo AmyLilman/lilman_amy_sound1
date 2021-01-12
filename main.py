f7bc4dd6ba6110b08763cc6f4b659193ff4a4758
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-01-12 10:15:26 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 while True:
     print("Sound is: " + input.sound_level())
     if input.sound_level() < 6:
-        light.set_all(light.rgb(0, 0, 255))
+        light.set_pixel_color(0, light.rgb(255, 100, 0))
+        light.set_pixel_color(1, light.rgb(245, 255, 0))
+        light.set_pixel_color(2, light.rgb(20, 245, 20))
+        light.set_pixel_color(3, light.rgb(66, 245, 230))
+        light.set_pixel_color(4, light.rgb(103, 181, 199))
+        light.set_pixel_color(5, light.rgb(66, 188, 245))
+        light.set_pixel_color(6, light.rgb(80, 123, 242))
+        light.set_pixel_color(7, light.rgb(190, 95, 227))
+        light.set_pixel_color(8, light.rgb(245, 66, 212))
+        light.set_pixel_color(9, light.rgb(242, 48, 78))
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
 while (true) {
     console.log("Sound is: " + input.soundLevel())
     if (input.soundLevel() < 6) {
-        light.setAll(light.rgb(0, 0, 255))
+        light.setPixelColor(0, light.rgb(255, 100, 0))
+        light.setPixelColor(1, light.rgb(245, 255, 0))
+        light.setPixelColor(2, light.rgb(20, 245, 20))
+        light.setPixelColor(3, light.rgb(66, 245, 230))
+        light.setPixelColor(4, light.rgb(103, 181, 199))
+        light.setPixelColor(5, light.rgb(66, 188, 245))
+        light.setPixelColor(6, light.rgb(80, 123, 242))
+        light.setPixelColor(7, light.rgb(190, 95, 227))
+        light.setPixelColor(8, light.rgb(245, 66, 212))
+        light.setPixelColor(9, light.rgb(242, 48, 78))
     } else {
         light.clear()
     }
